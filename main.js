adde6518a462242f18ce444c1d47847c524e90aa
Rocio Cesar <rociolcesar@gmail.com>
2023-11-23 14:20:04 -0300

    Agregando js

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const jsConfetti = new JSConfetti()
+
+    jsConfetti.addConfetti({
+        emojis: ['🌊','🏄🏻‍♂️','🐚','🏖️','🎣'],
+        emojiSize: 30,
+        confettiNumber: 300,
+    })
+}) 
